92cbf0c913e30dfe546998285c8d758840161034
vinaymeruva <vinaymeruva@gmail.com>
2023-01-05 22:57:43 +0530

    catalouge file added to roboshop project

--- catalouge.sh
+++ catalouge.sh
@@ -23,3 +23,7 @@ systemctl daemon-reload
 
 systemctl enable catalogue
 systemctl start catalogue
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org-shell -y
