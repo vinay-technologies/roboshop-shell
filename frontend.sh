b2f65936052b82f44bd2fed98b0db86e2f988de0
vinaymeruva <vinaymeruva@gmail.com>
2023-01-05 18:02:20 +0530

    fontend.sh file added to roboshop project

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,6 @@
+script_location=$(pwd)
+
+
 yum install nginx -yum
 systemctl start nginx
 systemctl enable nginx
@@ -10,7 +13,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 
 
