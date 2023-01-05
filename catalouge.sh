25208d36ad8b48512774ae36c8f59bd8ee1ea711
vinaymeruva <vinaymeruva@gmail.com>
2023-01-05 22:42:47 +0530

    catalouge file added to roboshop project

---
+++ catalouge.sh
@@ -0,0 +1,23 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+script_location=$(pwd)
+
+cp ${script_location}/files/catalouge.service /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+systemctl start catalogue
