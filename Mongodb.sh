d237573f1e026581541b3ef5b22470564e554819
vinaymeruva <vinaymeruva@gmail.com>
2023-01-05 22:29:08 +0530

    Mongod.sh file added to roboshop project

--- Mongodb.sh
+++ Mongodb.sh
@@ -5,5 +5,8 @@ cp ${script_location}/files/Mongodb.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
+sed -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
